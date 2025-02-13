a63df6fe1987d0db65c106085e9e78cae4aa92b1
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-02-13 00:09:19 -0300

    feat: configura pipeline e pirmiero Pcollection

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io import ReadFromText
 
-with beam.Pipeline() as pipeline:
-    (pipeline
-     | 'Create Data' >> beam.Create(['Hello', 'Apache Beam', 'Windows'])
-     | 'Print' >> beam.Map(print))
+pipeline_options = PipelineOptions(argc=None)
+pipeline = beam.Pipeline(options=pipeline_options)
+
+dengue = (
+    pipeline
+    | 'Leitura do dataset de dengue' >> ReadFromText('D:\\Projetos\\Python\\Data_Engineering\\beam_pipeline\\data\\casos_dengue.txt', skip_header_lines=1)
+
+)
+ 
